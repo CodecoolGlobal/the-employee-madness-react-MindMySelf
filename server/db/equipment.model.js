2852cee78949e5205e715ed17e888a05fc458e0f
csege <cseloteigergely@gmail.com>
2023-02-03 13:06:18 +0100

    add equipments to backend

--- server/db/equipment.model.js
+++ server/db/equipment.model.js
@@ -6,10 +6,6 @@ const EquipmentSchema = new Schema({
     name: String,
     type: String,
     amount: Number,
-    created: {
-        time: Date,
-        default: Date.now,
-    },
 });
 
 module.exports = mongoose.model("Equipment", EquipmentSchema);
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const EmployeeModel = require("./db/employee.model");
+const EquipmentModel = require("./db/equipment.model")
 
 const { MONGO_URL, PORT = 8080 } = process.env;
 
@@ -77,12 +78,74 @@ app.get("/robert", async (req, res) => {
   return res.json(roberts);
 })
 
+//equipment
+
+app.use("/api/equipments/:id", async (req, res, next) => {
+  let equipment = null;
+
+  try {
+    equipment = await EquipmentModel.findById(req.params.id);
+  } catch (err) {
+    return next(err);
+  }
+
+  if (!equipment) {
+    return res.status(404).end("Employee not found");
+  }
+
+  req.equipment = equipment;
+  next();
+});
+
+app.get("/api/equipments/", async (req, res) => {
+  const equipments = await EquipmentModel.find().sort({ created: "desc" });
+  return res.json(equipments);
+});
+
+app.get("/api/equipments/:id", (req, res) => {
+  return res.json(req.equipment);
+});
+
+app.post("/api/equipments/", async (req, res, next) => {
+  const equipment = req.body;
+
+  try {
+    const saved = await EquipmentModel.create(equipment);
+    return res.json(saved);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.patch("/api/equipments/:id", async (req, res, next) => {
+  const equipment = req.body;
+
+  try {
+    const updated = await req.equipment.set(equipment).save();
+    return res.json(updated);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.delete("/api/equipments/:id", async (req, res, next) => {
+  try {
+    const deleted = await req.equipment.delete();
+    return res.json(deleted);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+//
+
 const main = async () => {
   await mongoose.connect(MONGO_URL);
 
   app.listen(PORT, () => {
     console.log("App is listening on 8080");
     console.log("Try /api/employees route right now");
+    console.log("Try /api/equipments route right now");
   });
 };
 
